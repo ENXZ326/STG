2958af11d92c85fb78d53f4a100b58c800743d57
GSZ <hj-g18012@sist.ac.jp>
2019-09-27 12:08:41 +0900

    FPSクラス追加

---
+++ FPS.hpp
@@ -0,0 +1,41 @@
+#pragma once
+//########## ヘッダーファイル読み込み ##########
+#include "DxLib.h"
+
+//########## クラスの定義 ##########
+class FPS
+{
+private:
+	int			value;					//FPS値
+	double		drawValue;				//DrawするときのFPS値
+	DWORD		calcStartTime;			//0フレーム目の開始時刻
+	DWORD		calcEndTime;			//設定したフレームの終了時刻
+	DWORD		flameCount;				//フレームのカウント
+	double		calcAverage; 			//FPSを計算するための平均サンプル数
+
+
+public:
+	//コンストラクタ
+	//引　数：int　：FPS値
+	FPS(int);
+
+	//指定したFPSになるように待つ
+	VOID Wait(VOID);
+
+	//画面更新の時刻を取得する
+	VOID Update(VOID);
+
+	//FPSの値を表示する
+	//引　数：int　：値を表示するX位置
+	//引　数：int　：値を表示するY位置
+	VOID Draw(int, int);
+
+	//FPS値を取得する
+	int Getvalue(VOID);
+
+	//デストラクタ
+	virtual ~FPS();
+};
+
+//########## 外部オブジェクト ##########
+extern FPS *fps;
